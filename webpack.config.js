ba555bb3eadc39c98fcabe4750412b9a6b2ce23e
zhuangZhou <1851055435@163.com>
2017-09-22 07:15:41 +0800

    热更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,30 @@
 const path = require('path');
 
-module.exports={
+module.exports = {
     //入口文件的配置项
-    entry:{
-        entry:'./src/entery.js',
-        entry2:'./src/entery2.js'
+    entry: {
+        entry: './src/entery.js',
+        entry2: './src/entery2.js'
     },
     //出口文件的配置项
-    output:{
-        path:path.resolve(__dirname,'dist'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
         // filename:'bundle.js'
-        filename:'[name].js'
+        filename: '[name].js'
     },
     //模块，主要是解读css和图片转换压缩等功能
-    module:{},
+    module: {},
     //配置插件，用于生产模块和各项功能
-    plugins:[],
+    plugins: [],
     //配置webpack开发服务功能
-    devServer:{}
+    devServer: {
+        //配置服务的基本路径
+        contentBase: path.resolve(__dirname, 'dist'),
+        //服务器的IP地址
+        host: 'localhost',
+        //服务器压缩是否开启
+        compress: true,
+        //配置服务器的端口
+        port: 1608
+    }
 }
